bf876f4b79bca24b8aba2b50a25b8647eb98089a
yashbellam97 <yashbellam97@gmail.com>
2020-09-26 10:02:31 -0400

    Add OnClickListener to FAB

--- app/src/main/java/com/here/MainActivity.java
+++ app/src/main/java/com/here/MainActivity.java
@@ -1,14 +1,46 @@
 package com.here;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private TextInputLayout messageInputLayout;
+    private TextInputEditText messageInputEditText;
+    private FloatingActionButton sendMessageFab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        messageInputLayout = findViewById(R.id.messageInputLayout);
+        messageInputEditText = findViewById(R.id.messageInputEditText);
+        sendMessageFab = findViewById(R.id.sendMessageFab);
+
+        sendMessageFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CharSequence typedMessage = messageInputEditText.getText();
+                if (typedMessage != null) {
+                    String userMessage = typedMessage.toString().trim();
+                    if (!userMessage.trim().isEmpty()) {
+                        sendMessage(userMessage);
+                    }
+
+                }
+            }
+        });
+    }
+
+    private void sendMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
